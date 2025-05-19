1a0f148f8579b23cf4a6a1dbcded35d2349a323f
modirishabh <139623293+modirishabh@users.noreply.github.com>
2025-05-19 21:54:54 +0530

    Update module.tf

--- 2.module-gcp/module.tf
+++ 2.module-gcp/module.tf
@@ -19,3 +19,34 @@ module "network" {
 
   }
 }
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "2.0.2"
+
+  network_name = "terraform-vpc-network"
+  project_id   = var.project
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = var.cidr
+      subnet_region = var.region
+
+    },
+  ]
+
+  secondary_ranges = {
+    subnet-01 = []
+    
+  }
+}
+    
+module "network_fabric-net-firewall" {
+  source  = "terraform-google-modules/network/google//modules/fabric-net-firewall"
+  version = "1.1.0"
+  project_id              = var.project
+  network                 = module.network.network_name
+  internal_ranges_enabled = true
+  internal_ranges         = var.cidr
+
+}
