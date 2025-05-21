b11310dfd2468b09b62efbacc5c54ec56f3001d5
modirishabh <139623293+modirishabh@users.noreply.github.com>
2025-05-21 23:13:21 +0530

    Create variable.tf

---
+++ AWS/Day-3/module/ec2/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = "value for ami"
+      
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for subnet"
+  
+}
